b93facc54a1a30f74a9d47b9863d1dd810d5dc81
lego man <12103838+rwilliaise@users.noreply.github.com>
2022-01-15 21:33:57 -0800

    new tests

---
+++ helper-libs/helper.lua
@@ -0,0 +1,9 @@
+local libs = {}
+
+libs.testValue = 42
+
+function libs.testFunction()
+    return "Hello from helper-libs"
+end
+
+return libs
--- test-pkg/main.lua
+++ test-pkg/main.lua
@@ -1,1 +1,6 @@
-print("Hello!")
+print("Hello, world!")
+
+local libs = require("helper-libs/helper.lua")
+
+print(libs.testValue)
+print(libs.testFunction())
